9bfb8d2fa6196715b155bd8584e76ed9c08165ca
Kwon, Oh-Hyun <ohyun.kwon@gmail.com>
2018-10-11 13:51:34 +0900

    first commit

---
+++ src/components/SimpleDisplay.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="mx-5 border border-dark p-2">
+        <h3 class="bg-warning text-white text-center p-2">Display</h3>
+
+        <transition enter-to-class="fadeIn" leave-to-class="fadeOut" mode="out-in">
+            <div v-if="show" class="animated h4 bg-info text-center p-2" key="hello">Hello, Adam</div>
+            <div v-else class="animated h4 bg-success text-center p-2" key="goodbye">Goodbye, Adam</div>
+        </transition>
+
+        <div class="text-center">
+            <button v-on:click="toggle" class="btn btn-primary">Toggle Visibility</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SimpleDisplay",
+        data() {
+            return {
+                show: true
+            }
+        },
+        methods: {
+            toggle() {
+                this.show = !this.show;
+            }
+        }
+    }
+</script>
+
+<style>
+    .v-leave-active {
+        opacity: 0;
+        font-size: 0em;
+        transition: all 250ms;
+    }
+    .v-enter {
+        opacity: 0;
+        font-size: 0em;
+    }
+    .v-enter-to {
+        opcity: 1;
+        font-size: x-large;
+        transition: all 250ms;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
+import 'popmotion/dist/popmotion.global.min';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
